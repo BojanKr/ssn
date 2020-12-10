5a94cd5cdca6d50fc25f6ad91eedb1f914ee9345
bk <bojankrtenic@gmail.com>
2020-12-10 09:15:40 +0100

    main

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ from py2cytoscape import cyrest
 import os
 
 # Create folders
-file = os.listdir(os.path.join(os.getcwd(), 'network/'))[0]
-print(file)
 for file in os.listdir(os.path.join(os.getcwd(), 'network/')):
-    project_folder = os.path.join(os.getcwd(), file+'/')
-    if not os.path.exists(project_folder):
-        os.makedirs(project_folder)
+    if not file.endswith('xgmml'):
+        continue
     else:
-        new_name = project_folder[:-1]+'_copy_'+str(1)+'/'
-        if not os.path.exists(new_name):
-            os.rename(project_folder, new_name)
+        project_folder = os.path.join(os.getcwd(), file+'/')
+        if not os.path.exists(project_folder):
+            os.makedirs(project_folder)
         else:
-            directory_list = []
-            for directory in os.listdir(os.getcwd()):
-                if directory.startswith(file):
-                    directory_list.append(directory)
-            os.rename(project_folder, project_folder[:-1]+'_copy_'+str(len(directory_list)))
-            print(f'Folder {file} already exists. It has been renamed to {file}_copy_{str(len(directory_list))}')
-        os.makedirs(project_folder)
+            new_name = project_folder[:-1]+'_copy_'+str(1)+'/'
+            if not os.path.exists(new_name):
+                os.rename(project_folder, new_name)
+            else:
+                directory_list = []
+                for directory in os.listdir(os.getcwd()):
+                    if directory.startswith(file):
+                        directory_list.append(directory)
+                os.rename(project_folder, project_folder[:-1]+'_copy_'+str(len(directory_list)))
+                print(f'Folder {file} already exists. It has been renamed to {file}_copy_{str(len(directory_list))}')
+            os.makedirs(project_folder)
     project_results_folder = project_folder + 'results/'
     project_data_folder = project_folder + 'data/'
     os.makedirs(project_results_folder)
@@ -40,35 +41,47 @@ cytoscape = cyrest.cyclient()
 # Load network to Cytoscape
 print('Loading network to Cytoscape')
 for file in os.listdir(os.path.join(os.getcwd(), 'network/')):
-    network_path = os.path.join(os.getcwd(), 'network', file)
-    cytoscape.network.load_file(network_path)
-print('Network loaded successfully')
+    if not file.endswith('xgmml'):
+        continue
+    else:
+        network_path = os.path.join(os.getcwd(), 'network', file)
+        try:
+            cytoscape.network.load_file(network_path)
+            print('Network loaded successfully')
+        except:
+            continue
 
-# calculate layout
-print('Applying favourite layout')
-cytoscape.layout.apply_preferred()
-print('Layout applied successfully')
+# Create view and apply layout
+views = cytoscape.view.list()
+if not views['views']:
+    print('Creating view')
+    cytoscape.view.create()
+    print('Applying favourite layout')
+    cytoscape.layout.apply_preferred()
+else:
+    print('Applying favourite layout')
+    cytoscape.layout.apply_preferred()
 
 # Default visual style
 cytoscape.vizmap.apply(styles='default')
 
 # Run the first rule (run_clusterONE)
-print(f'Running clusterONE algorithm and extracting general information about the full {file} SSN.\n')
+print(f'Running clusterONE algorithm and extracting general information about the full SSN.\n')
 os.system('snakemake -s rules/run_clusterONE.rules --cores 1')
 print('Step complete.\n')
 
 
 # Run the second rule (to get information per cluster)
-print(f'Extracting information for each defined cluster from the {file} SSN.\n')
+print(f'Extracting information for each defined cluster from the SSN.\n')
 os.system('snakemake -s rules/cluster_info.rules --cores 1')
 print('Step complete.\n')
 
 # Run the third rule (to calculate seqeunce motifs)
-print(f'Running MEME from MEME-Suite to calculate sequence motifs for defined clusters from the {file} SSN.\n')
+print(f'Running MEME from MEME-Suite to calculate sequence motifs for defined clusters from the SSN.\n')
 os.system('snakemake -s rules/sequence_motifs.rules --cores 1')
 print('Step complete.\n')
 
 # Run the found rule (search calculated fingerprints against the dataset)
-print(f'Running MAST from MEME-Suite to search previously calculated sequence motifs against the dataset used to calculate the {file} SSN.\n')
+print(f'Running MAST from MEME-Suite to search previously calculated sequence motifs against the dataset used to calculate the SSN.\n')
 os.system('snakemake -s rules/run_mast.rules --cores 1')
 print('Step complete.\n')
